277bc2769ff3bd70f4d6fce4b4d8aa586cd982ed
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-12-10 20:47:11 +0000

    print to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    for a in range(0, len(str)):
+        stru = ord(str[a])
+        if stru > 95:
+            stru = stru - 32
+        print("{:c}".format(stru), end="")
+    print("")
