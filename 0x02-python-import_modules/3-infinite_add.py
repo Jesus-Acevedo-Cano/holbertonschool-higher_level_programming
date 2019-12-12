6544befc4bc624afc19c27019a4e4ffb61071c8b
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-12-12 19:58:56 +0000

    fix infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if (__name__ == "__main__"):
-   add = 0
-   for a in range(1, len(sys.argv)):
+    add = 0
+    for a in range(1, len(sys.argv)):
         add += int(sys.argv[a])
-   print("{:d}".format(add))
+    print("{:d}".format(add))
