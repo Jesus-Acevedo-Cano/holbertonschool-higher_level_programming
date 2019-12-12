d1e5aec1a9881388c1a83fad5f253798d4bcf18e
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-12-12 22:08:28 +0000

    my func calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if (__name__ == "__main__"):
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    args = len(sys.argv)
+    if (args != 4):
+        print(args)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    ops = sys.argv[2]
+    if (ops == "+"):
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif (ops == "-"):
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif (ops == "*"):
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif (ops == "/"):
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
