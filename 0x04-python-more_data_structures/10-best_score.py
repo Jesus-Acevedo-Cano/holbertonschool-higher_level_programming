aa1589d71ca9028b08a30442e0175afd252d654c
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-12-19 22:58:18 +0000

    the best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        new = sorted(a_dictionary)
-        return max(new)
+        new = max(a_dictionary)
+        return new
     return None
