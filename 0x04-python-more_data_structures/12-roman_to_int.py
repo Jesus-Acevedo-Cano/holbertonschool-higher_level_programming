dd75801dc95227ac4a774b57656677d9493bde87
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-12-20 00:52:05 +0000

    fix conditions

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    cp = roman_string
-    conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    res = 0
-    for j in range(len(cp)):
-        for i, val in conv.items():
-            if i == cp[j]:
-                if j + 1 < len(cp) and conv[cp[j + 1]] > conv[cp[j]]:
-                    res -= val
-                else:
-                    res += val
-    return res
+    if roman_string and type(roman_string) == str:
+        cp = roman_string
+        conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = 0
+        for j in range(len(cp)):
+            for i, val in conv.items():
+                if i == cp[j]:
+                    if j + 1 < len(cp) and conv[cp[j + 1]] > conv[cp[j]]:
+                        res -= val
+                    else:
+                        res += val
+        return res
+    return 0
