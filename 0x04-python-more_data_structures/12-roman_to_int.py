d63b3148326f8b1919554f118506d9b81bb3f8ee
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2019-12-20 00:54:14 +0000

    fix conditions and pep

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if roman_string and type(roman_string) == str:
         cp = roman_string
-        conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        con = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         res = 0
         for j in range(len(cp)):
-            for i, val in conv.items():
+            for i, val in con.items():
                 if i == cp[j]:
-                    if j + 1 < len(cp) and conv[cp[j + 1]] > conv[cp[j]]:
+                    if j + 1 < len(cp) and con[cp[j + 1]] > con[cp[j]]:
                         res -= val
                     else:
                         res += val
