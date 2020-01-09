45248d5cbe61b5e08c73d9a5217b16f6978ff74e
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-01-09 20:40:12 +0000

    pep8 :/

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square():
     """class named square"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Initialization of instance attributes
            Args:
            size (int): The size of the square
