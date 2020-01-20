b6105ea5f83aafeeb2518e8a7440e8cb7cdd37bb
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-01-20 20:03:11 +0000

    inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""subclass module"""
+
+
+def inherits_from(obj, a_class):
+    """
+       function that returns True if the object is an instance of a
+       class that inherited (directly or indirectly) from the specified class;
+       otherwise False.
+       Args:
+            obj: object
+            a_class: class
+            return: True or False
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
