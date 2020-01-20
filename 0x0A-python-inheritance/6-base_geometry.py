bb97f6a1e6e9ee30b809ecaca838da118be5dd2c
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-01-20 20:48:32 +0000

    raise an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""create a public instance"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
