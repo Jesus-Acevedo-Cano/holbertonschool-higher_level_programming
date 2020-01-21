66e1c01b897086a418271475b7f1e038192fa27f
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-01-21 01:49:17 +0000

    type and value errors

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""create a public instance"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """validate value"""
+        if type(value) is not int:
+            raise TypeError ("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError ("{:s} must be greater than 0".format(name))
