b08150ef129e1eb8da61dbf9399df7134a7eb3bc
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-01-21 22:22:50 +0000

    to json

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+""" JSON representation of an object"""
+
+
+def to_json_string(my_obj):
+    """ function that returns JSON representation of an object """
+    return json.dumps(my_obj)
