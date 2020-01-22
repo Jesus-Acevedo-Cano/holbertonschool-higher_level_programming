a58e7a832312456696e0b55956afd310d5c9cd0c
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-01-22 00:55:58 +0000

    student 3.0

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" student class module """
+
+
+class Student():
+    """ Class Student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            new = [a for a in attrs if hasattr(self, a)]
+            return {a: getattr(self, a) for a in new}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            self.__dict__[key] = value
