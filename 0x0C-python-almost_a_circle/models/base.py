b95137ea011df668bc2bd6d976dbd1bf6a4be0c7
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-01-26 19:06:55 +0000

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,10 +6,11 @@ class Base:
     """ Base class """
     __nb_objects = 0
 
-def __init__(self, id=None):
-    """ init constructor """
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        """ init constructor """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
