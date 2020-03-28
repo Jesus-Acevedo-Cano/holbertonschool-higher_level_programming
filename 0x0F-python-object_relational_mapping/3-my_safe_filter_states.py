2b82f12cc112ed24ea44116ba5ad8572d47597ef
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-28 22:47:41 +0000

    task 3 done

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script that takes an argument and displays all values in the states table"""
+
+import sys
+import MySQLdb
+"""modules import"""
+
+
+if __name__ == '__main__':
+    """arguments order"""
+    user = sys.argv[1]
+    pswrd = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=user,
+        passwd=pswrd,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE BINARY " +
+                   "name = %s ORDER BY states.id ASC", (search, ))
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    cursor.close()
+    db.close()
