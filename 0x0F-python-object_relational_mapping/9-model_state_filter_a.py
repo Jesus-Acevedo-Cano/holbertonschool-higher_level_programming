b8ef3831f8cdc24000b667f7ff9b6c08919f2abb
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-29 07:24:15 +0000

    only states with a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+"""modules import"""
+
+if __name__ == '__main__':
+    """arguments order"""
+    user = sys.argv[1]
+    pswrd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(user, pswrd, db),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    seek = State.name.like(func.binary('%a%'))
+    result = session.query(State).filter(seek).order_by(State.id).all()
+    for row in result:
+        print("{}: {}".format(row.id, row.name))
+    session.close()
