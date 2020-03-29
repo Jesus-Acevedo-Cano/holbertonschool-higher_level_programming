a7230091a75c25c1176ecfd8ee2b3b7403232196
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-03-29 07:40:02 +0000

    only state pased by arg

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+"""modules import"""
+
+if __name__ == '__main__':
+    """arguments order"""
+    user = sys.argv[1]
+    pswrd = sys.argv[2]
+    db = sys.argv[3]
+    search = sys.argv[4]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(user, pswrd, db),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.query(State).filter_by(name=search).order_by(State.id)
+    if not result:
+        print("Not found")
+    for row in result:
+        print("{}".format(row.id))
+    session.close()
