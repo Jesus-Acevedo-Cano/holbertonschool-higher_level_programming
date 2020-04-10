ec27962507e35e230ca31bac620626d662261ca3
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-04-10 01:53:20 +0000

    find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" finds a peak in a list of unsorted integers """
+
+def find_peak(list_of_integers):
+    if len(list_of_integers) < 1:
+        return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+    else:
+        return max(list_of_integers)
