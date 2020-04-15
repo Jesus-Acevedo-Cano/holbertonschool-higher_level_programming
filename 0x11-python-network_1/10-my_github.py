8fcddc6dd6dd63db3bfe8feb29f1dd9149a60088
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-04-15 06:30:49 +0000

    my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# takes in a letter and sends a POST request
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+    print(r.json.get("id"))
