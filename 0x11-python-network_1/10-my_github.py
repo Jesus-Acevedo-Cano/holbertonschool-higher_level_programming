51ecab6871eaea148644141a8513bed3aa804fc0
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-04-17 03:26:13 +0000

    correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,4 +6,4 @@ from sys import argv
 
 if __name__ == '__main__':
     r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    print(r.json.get("id"))
+    print(r.json().get("id"))
