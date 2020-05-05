b983aeebb09f310520229fdf2c19c58fa0be41ce
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-05-05 22:50:48 +0000

    print 3 lineswith a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that prints 3 lines with a loop
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i;
+for (i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
