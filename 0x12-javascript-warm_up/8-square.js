6ade698673b239eee13c9c2e14bb140cb88ddf28
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-05-05 23:58:03 +0000

    xquare

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,6 +5,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (i = 0; i < process.argv[2]; i++) {
-    console.log('x'.repeat(process.argv[2]));
+    console.log('X'.repeat(process.argv[2]));
   }
 }
