29405c46d1ff145a50224bc8d8d639ea3bf153ba
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-05-05 20:48:29 +0000

    print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // script that prints 3 lines
-const myVar1 = 'C is fun'
-const myVar2 = 'Python is cool'
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
 const myVar3 = 'Javascript is amazing';
 console.log("%s\n%s\n%s", myVar1, myVar2, myVar3);
