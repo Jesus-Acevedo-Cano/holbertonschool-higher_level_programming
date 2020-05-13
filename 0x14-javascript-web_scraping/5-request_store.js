94eee2dd498adcbd3b49d9f039175beedf263f1e
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-05-13 01:49:35 +0000

    write body response on a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,10 +6,10 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    fs.writeFile(process.argv[3], body, function(error) {
+    fs.writeFile(process.argv[3], body, function (error) {
       if (error) {
         console.log(error);
       }
-    })
+    });
   }
 });
