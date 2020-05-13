a39fda822bea26cfc6c01d908033a88e3af1855f
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2020-05-13 01:18:59 +0000

    number of movies where character apears

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+//  prints number of movies where character apears
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let cnt = 0;
+    JSON.parse(body).results.forEach(character => {
+      character.characters.forEach(link => {
+        if (link.endsWith('/18/')) {
+          cnt++;
+        }
+      });
+    });
+    console.log(cnt);
+  }
+});
